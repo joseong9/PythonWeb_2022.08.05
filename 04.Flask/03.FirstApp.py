4804608d63974647ea6c1e297ee0a2f65669dfe3
joseong9 <jsg8825@gamil.com>
2022-09-06 14:54:29 +0900

    new_file

--- 04.Flask/03.FirstApp.py
+++ 04.Flask/03.FirstApp.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template('01.index.html')
 
-@app.route('/menu', method=['GET','POST'])
+@app.route('/menu', methods=['GET','POST'])
 def menu():
     if request.method == 'GET':
-        return render_template('02.menu.html')
+        return render_template('02.Menu.html')
     else:
-        return render_template('03.menu_res.html')
+        return render_template('03.Menu_res.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
